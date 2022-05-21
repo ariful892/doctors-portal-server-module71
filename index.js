eeb7330698034efaf4faeafc845ecdd090a1782b
ariful892 <ariful00892@gmail.com>
2022-05-21 16:55:27 +0600

    edited admin api

--- index.js
+++ index.js
@@ -50,11 +50,11 @@ async function run() {
             res.send(users);
         })
 
-        app.get('admin/:email', async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
-            res.send({ admin: isAdmin });
+            res.send({ admin: isAdmin })
         })
 
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
